2b5d62d77e2a09d1604814561b3ac007a485e987
Gnatannn <96788997+Gnatannn@users.noreply.github.com>
2022-07-30 19:41:51 +0300

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,63 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const imgMarkup = galleryItems
+  .map((galleryItem) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+  <img
+  class="gallery__image"
+  src="${galleryItem.preview}"
+  data-sourse="${galleryItem.original}"
+  alt="${galleryItem.description}"/>
+  </a>
+  </div>`;
+  })
+  .join("");
+
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("afterbegin", imgMarkup);
+gallery.addEventListener("click", onImgClick);
+
+// let instance;
+
+// function onModalClose() {
+//   instance.close();
+// }
+
+// function onEscClose(event) {
+//   const ESC_KEY_CLOSE = "Escape";
+//   if (event.code === ESC_KEY_CLOSE) {
+//     onModalClose();
+//   }
+// }
+
+function onImgClick(event) {
+  //   console.log(event.target.dataset.sourse);
+  event.preventDefault();
+  const instance = basicLightbox.create(
+    `
+      <img src="${event.target.dataset.sourse}">`,
+    {
+      onShow: (instance) => {
+        // window.addEventListener("keydown", onEscClose);
+        document.onkeydown = function (evt) {
+          evt = evt || window.event;
+          var isEscape = false;
+          if ("key" in evt) {
+            isEscape = evt.key === "Escape" || evt.key === "Esc";
+          } else {
+            isEscape = evt.keyCode === 27;
+          }
+          if (isEscape) {
+            instance.close();
+          }
+        };
+      },
+      //   onClose: (instance) => {
+      //     window.removeEventListener("keydown", onEscClose);
+      //   },
+    }
+  );
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const imgMarkup = galleryItems
+  .map((galleryItem) => {
+    return `<a class="gallery__item" href="${galleryItem.original}">
+  <img
+  class="gallery__image"
+  src="${galleryItem.preview}"
+  alt="${galleryItem.description}"/>
+  </a>`;
+  })
+  .join("");
+
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("afterbegin", imgMarkup);
+gallery.addEventListener("click", onImgClick);
+
+function onImgClick(event) {
+  event.preventDefault();
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionType: "attr",
+    captionPosition: "bottom",
+    captionDelay: 250,
+    captionsData: "alt",
+  });
+}
